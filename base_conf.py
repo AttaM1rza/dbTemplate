f14fa28c07d581f3a27e3636ea121cdab0187d52
Atta Mirza <atta.mirza@outlook.de>
2023-03-04 09:00:16 +0100

    INIT COMMIT

--- base_conf.py
+++ base_conf.py
@@ -7,10 +7,10 @@ configFile = os.path.join(BASE_DIR, "config.ini")
 # Load environment variables
 config = configparser.ConfigParser()
 config.read("config.ini")  # todo make it better (independet with config file)
-
+print(config.get("database", "username"))
 # get database configurations
 DATABASE = {
-    'USERNAME': config['database']['username'],
+    'USERNAME': config.get("database", "username"),
     'PASSWORD': config['database']['password'],
     'HOST': config['database']['host'],
     'PORT': config['database']['port'],
