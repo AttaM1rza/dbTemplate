c7cbf33c604a3c2ac81c850701166d45e063be43
Atta Mirza <atta.mirza@outlook.de>
2023-03-07 23:31:35 +0100

    INIT COMMIT

--- conf.py
+++ conf.py
@@ -3,8 +3,11 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# FOLDERS
+settingsFolder = os.path.join(BASE_DIR, "settings")
+
 # FILES
-configFile = os.path.join(BASE_DIR, "settings/config.ini")
+configFile = os.path.join(settingsFolder, "config.ini")
 
 # ENVIRONMENT VARIABLES
 config = configparser.ConfigParser()
